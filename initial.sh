09c1df1351d8501ee5f896c23a3bddee573dc26a
Deepak <oyedeepakkumar@gmail.com>
2021-05-03 22:14:22 +0530

    Add files via upload

---
+++ initial.sh
@@ -0,0 +1,13 @@
+sudo apt install vim
+sudo apt install vscode
+sudo apt install pulseaudio
+sudo apt install pavucontrol
+sudo apt install qbittorrent
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo apt install vlc
+sudo apt install gnome-tweaks # After that make caps additional esc key
+sudo apt install steam
+sudo apt-get install gnome-shell-pomodoro
+sudo apt install shotwell
+
